2e8a861c86303dab362d457c9f9f0483043d2ec3
maaaarcrx <136459469+maaaarcrx@users.noreply.github.com>
2026-06-01 14:45:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   nitro: {
-    preset: "netlify",
+    preset: "vercel",
   },
   tanstackStart: {
     server: { entry: "server" },
